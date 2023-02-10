7d3aae54200cb210702f4657c8332832ef89bd3c
staglibrary <macgregor.pr@gmail.com>
2023-02-10 15:52:11 +0000

    Update from https://github.com/staglibrary/stag/commit/2dc269ec0781d6e0ae2f388d16c88f2181932a44

--- docs/cpp/navtreedata.js
+++ docs/cpp/navtreedata.js
@@ -27,31 +27,36 @@ var NAVTREE =
   [ "STAG C++", "index.html", [
     [ "Getting Started", "getting-started.html", [
       [ "Installation", "getting-started.html#autotoc_md0", null ],
-      [ "Constructing Graphs", "getting-started.html#autotoc_md1", null ]
+      [ "Constructing Graphs", "getting-started.html#autotoc_md1", [
+        [ "Named Graphs", "getting-started.html#autotoc_md2", null ],
+        [ "Adjacency Matrices", "getting-started.html#autotoc_md3", null ],
+        [ "Loading Graphs from Disk", "getting-started.html#autotoc_md4", null ],
+        [ "Random Graphs", "getting-started.html#autotoc_md5", null ]
+      ] ]
     ] ],
     [ "Changelog", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html", [
-      [ "[Unreleased]", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md3", [
-        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md4", null ]
+      [ "[Unreleased]", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md7", [
+        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md8", null ]
       ] ],
-      [ "[0.4.0] - 2023-01-16", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md5", [
-        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md6", null ],
-        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md7", null ]
+      [ "[0.4.0] - 2023-01-16", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md9", [
+        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md10", null ],
+        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md11", null ]
       ] ],
-      [ "[0.3.0] - 2022-11-18", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md8", [
-        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md9", null ],
-        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md10", null ]
+      [ "[0.3.0] - 2022-11-18", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md12", [
+        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md13", null ],
+        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md14", null ]
       ] ],
-      [ "[0.2.1] - 2022-10-19", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md11", [
-        [ "Changed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md12", null ],
-        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md13", null ],
-        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md14", null ]
-      ] ],
-      [ "[0.2.0] - 2022-10-15", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md15", [
+      [ "[0.2.1] - 2022-10-19", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md15", [
         [ "Changed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md16", null ],
-        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md17", null ]
+        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md17", null ],
+        [ "Fixed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md18", null ]
       ] ],
-      [ "[0.1.6] - 2022-10-10", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md18", [
-        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md19", null ]
+      [ "[0.2.0] - 2022-10-15", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md19", [
+        [ "Changed", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md20", null ],
+        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md21", null ]
+      ] ],
+      [ "[0.1.6] - 2022-10-10", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md22", [
+        [ "Added", "md__home_runner_work_stag_stag_stag_lib_CHANGELOG.html#autotoc_md23", null ]
       ] ]
     ] ],
     [ "Classes", "annotated.html", "annotated" ],

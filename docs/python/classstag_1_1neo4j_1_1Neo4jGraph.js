912d1dff9bc462a253e2ab177106b2b9468edacb
staglibrary <macgregor.pr@gmail.com>
2023-03-22 14:59:28 +0000

    Update from https://github.com/staglibrary/stagpy/commit/8198a4145ad57fc39a0cc45bd7cdd797ec52b438

--- docs/python/classstag_1_1neo4j_1_1Neo4jGraph.js
+++ docs/python/classstag_1_1neo4j_1_1Neo4jGraph.js
@@ -3,8 +3,6 @@ var classstag_1_1neo4j_1_1Neo4jGraph =
     [ "__init__", "classstag_1_1neo4j_1_1Neo4jGraph.html#aebbad3b985992840d0976bb04dca6765", null ],
     [ "degree", "classstag_1_1neo4j_1_1Neo4jGraph.html#af80192c0eead41c7acaf18caa7787eb9", null ],
     [ "degree_unweighted", "classstag_1_1neo4j_1_1Neo4jGraph.html#a30c2418ebdb5be52b06b4a23b443f20f", null ],
-    [ "degrees", "classstag_1_1neo4j_1_1Neo4jGraph.html#ab4d642933cea09859645b26e148546ed", null ],
-    [ "degrees_unweighted", "classstag_1_1neo4j_1_1Neo4jGraph.html#a77949e7d81a9e373f7c205ee06f43112", null ],
     [ "neighbors", "classstag_1_1neo4j_1_1Neo4jGraph.html#ab760d10d107f4008c918dc1d2963468e", null ],
     [ "neighbors_unweighted", "classstag_1_1neo4j_1_1Neo4jGraph.html#a54f90aa69348e4b6ddcffa6f51cb5595", null ],
     [ "query_node_labels", "classstag_1_1neo4j_1_1Neo4jGraph.html#a573b9b93a4b3d634eead921d5eb50851", null ],

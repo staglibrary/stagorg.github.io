63a7cb80989483f6a7d4b2924f415abb2c4f398c
staglibrary <macgregor.pr@gmail.com>
2024-05-07 15:46:28 +0000

    Update from https://github.com/staglibrary/stag/commit/450a462e7336c5ada7a1be914015c9b18b66bf4f

--- docs/cpp/docs-2.0.0/search/functions_10.js
+++ docs/cpp/docs-2.0.0/search/functions_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['total_5fvolume_0',['total_volume',['../classstag_1_1Graph.html#a414f41da9d9ca4a44131d4d336c9a17f',1,'stag::Graph']]]
+  ['to_5fvector_0',['to_vector',['../classstag_1_1DataPoint.html#a96d7c56c037c994ed66d6184fdd88729',1,'stag::DataPoint']]],
+  ['total_5fvolume_1',['total_volume',['../classstag_1_1Graph.html#a414f41da9d9ca4a44131d4d336c9a17f',1,'stag::Graph']]]
 ];
